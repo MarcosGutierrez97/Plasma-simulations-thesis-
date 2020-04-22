e01f0a0ba891c79e04ebf2d96a4df9ca5c413c38
MarcosGutierrez97 <63207015+MarcosGutierrez97@users.noreply.github.com>
2020-04-22 00:19:43 -0600

    Add files via upload

---
+++ Corrección 1/plasma frio.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 16:02:51 2020
+
+@author: HP
+"""
+
+import funciones as f
+import parametros as pa
+import numpy as np
+
+
+#Ciclo inicial. Este solo se hace una vez
+
+x_inicial = f.buildgrid_pos()
+v_inicial = f.buildgrid_vel()
+densidad_inicial = f.chargedensity(x_inicial)
+E_inicial = f.electricfield(densidad_inicial)
+
+
+#Se empieza a constuir el ciclo
+
+temp = pa.dt
+
+while temp < 0.5:
+
+    #Mover particulas
+    posicion = f.chargeposition(v_inicial)
+    velocidad = f.chargevelocity(x_inicial,E_inicial)
