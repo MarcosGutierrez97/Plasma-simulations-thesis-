f84087925dc07b513e3ea14d06f263d3dd330b96
MarcosGutierrez97 <63207015+MarcosGutierrez97@users.noreply.github.com>
2020-04-28 19:08:00 -0600

    Revert "correct"
    This reverts commit 5664b6fe28d1c241e0c26b9621bb039c2d4a6e5a.

--- Corrección 1/plasma frio.py
+++ Corrección 1/plasma frio.py
@@ -8,13 +8,14 @@ Created on Wed Apr 15 16:02:51 2020
 import funciones as f
 import parametros as pa
 import numpy as np
-
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 #Ciclo inicial. Este solo se hace una vez
 
 
-x_inicial = f.buildgrid_pos() #Solo se hace una vez
-v_inicial = f.buildgrid_vel() #Solo se hace una vez
+x_inicial = f.buildgrid_pos(pa.x_inicial) #Solo se hace una vez
+v_inicial = f.buildgrid_vel(pa.v_inicial) #Solo se hace una vez
 densidad_inicial = f.chargedensity(x_inicial,pa.densidadE)
 E_inicial = f.electricfield(densidad_inicial)
 
@@ -23,10 +24,66 @@ E_inicial = f.electricfield(densidad_inicial)
 
 #Se empieza a constuir el ciclo
 
-temp = pa.dt
 
-# while temp < 0.5:  # proba esto despues, primero que te corra una vez
+temp = 0 #Contador de tiempo
+j = 0 #Contador en listas
+e = 1
+posiciones= []
+velocidades = []
+densidades = []
+camposE = []
+camposE_particulas = []
+tiempo = [0]
 
-    #Mover particulas t = 0
-posicion = f.chargeposition(v_inicial)
-velocidad = f.chargevelocity(x_inicial,v_inicial, E_inicial)
+
+while  temp < 1:
+    if temp == 0:
+        posicion = f.chargeposition( v_inicial,x_inicial)
+        posicion = f.cf(posicion)
+        velocidad = f.chargevelocity(x_inicial,v_inicial,E_inicial)
+        densidad = f.chargedensity(posicion, densidad_inicial)
+        E = f.electricfield(densidad)
+        posiciones.append(posicion)
+        velocidades.append(velocidad)
+        densidades.append(densidad)
+        camposE.append(E)
+        camposE_particulas.append([v/(pa.carga_e*pa.dt) for v in velocidades[j]])
+    else:
+        posicion = f.chargeposition(velocidad,posicion)
+        posicion = f.cf(posicion)
+        velocidad = f.chargevelocity(posicion,velocidad,E)
+        densidad = f.chargedensity(posicion, densidad)
+        E = f.electricfield(densidad)
+        posiciones.append(posicion)
+        velocidades.append(velocidad)
+        densidades.append(densidad)
+        camposE.append(E)
+        camposE_particulas.append([v/(pa.carga_e*pa.dt) for v in velocidades[j]])
+    temp = temp + pa.dt
+
+
+
+"""
+print ('lista posiciones')
+print (posiciones)
+print (len(posiciones))
+print (posiciones[0])
+print (len(posiciones[0]))
+print ('valores de v')
+print (velocidades)
+print (len(velocidades))
+print (velocidades[0])
+print (len(velocidades[0]))
+print ('valores de E')
+print (camposE)
+print (len(camposE))
+print (camposE[0])
+print(len(camposE[0]))
+"""
+
+#Densidad Neta (idea tomada del código del Cern)
+
+#plt.plot (pa.coor_malla,  camposE[2])
+#plt.xlabel('x')
+#plt.xlim(0,pa.malla_longitud)
+#plt.show()
