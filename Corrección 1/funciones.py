a4cec863226ad551a970f854780c007e30103e9b
MarcosGutierrez97 <63207015+MarcosGutierrez97@users.noreply.github.com>
2020-05-05 17:24:02 -0600

    Correcciones 05 de mayo
    Te puse todo lo que hice en whatsapp. También hice el cambio que me sugeriste hace un par de días y está nice :+1.

--- Corrección 1/funciones.py
+++ Corrección 1/funciones.py
@@ -10,6 +10,7 @@ import parametros as pa
 import numpy as np
 import scipy as sp
 from scipy import integrate
+import matplotlib.pyplot as plt
 #################################
 # Funciones para el ciclo del PIC
 #################################
@@ -68,13 +69,23 @@ def chargevelocity(x,v,E_malla):
     E_particula = []
     i = 0 #Contador para C_i
     for k in range (pa.noParticulas):
-        if x[k] >= pa.coor_malla[i] and x[k] <= pa.coor_malla[i + 1]:
-            c1 = ((pa.carga_e/pa.dx)*(pa.coor_malla[i + 1] - x[k])) #rho_i
-            c2 = ((pa.carga_e/pa.dx)*(x[k] - pa.coor_malla[i])) #rho_i+1
-            E_particula.append((E_malla[i]*c1 + E_malla[i+1] * c2))
+        xa = x[k]/pa.dx
+        j1 = int(xa)
+        print (j1)
+        j2 = j1 + 1
+        print (j2)
+        f2 = xa - j1
+        f1 = 1.0 - f2
+        ex = f1*E_malla[j1] + f2*E_malla[j2]
+        v[k] = v[k] + pa.carga_e*pa.dt*ex
+    '''
+    for k in range (pa.noParticulas):
+        if int(x[k]/pa.dx) >= pa.coor_malla[i] and int(x[k]/pa.dx) <= pa.coor_malla[i + 1]:
+            E_particula.append((E_malla[i]*(1-(int(x[k]/pa.dx)-i)) + E_malla[i+1] *(int(x[k]/pa.dx)-i)))
             v[k] = v[k] + pa.carga_e*E_particula[k]*pa.dt
         elif x[k] > pa.coor_malla[i + 1]:
             i = i + 1
+    '''
     return v
 
 def FieldParticle (x,E_malla): #Campo aplicado a cada particula
@@ -82,10 +93,8 @@ def FieldParticle (x,E_malla): #Campo aplicado a cada particula
     i = 0 #Contador para C_i
     #Acorde a la presentacion de plasma del CERN
     for k in range (pa.noParticulas):
-        if x[k] >= pa.coor_malla[i] and x[k] <= pa.coor_malla[i + 1]:
-            c1 = ((pa.carga_e/pa.dx)*(pa.coor_malla[i + 1] - x[k])) #rho_i
-            c2 = ((pa.carga_e/pa.dx)*(x[k] - pa.coor_malla[i])) #rho_i+1
-            E_a_particula.append((E_malla[i]*c1 + E_malla[i+1] * c2))
+        if (x[k]/pa.dx) >= pa.coor_malla[i] and (x[k]/pa.dx) <= pa.coor_malla[i + 1]:
+            E_a_particula.append((E_malla[i]*(1-((x[k]/pa.dx)-i)) + E_malla[i+1] *((x[k]/pa.dx)-i)))
 
         elif x[k] > pa.coor_malla[i + 1]:
             i = i + 1
@@ -126,19 +135,30 @@ def chargedensity(x):
     Solo si X[0] = 0 (revisa que esto suceda) YA SUCEDE
     '''
     charge_density = np.zeros(pa.noMalla + 1)
+    re = pa.carga_e/pa.dx
     i = 0 #Contador para C_i
     j = 1#Contador para C_i+1
     malla = 0 #Contador de nodos
-    k = 0
+    #k = 0
     #Acorde a la presentacion de plasma del CERN
-    while k < (pa.noParticulas):
-        if x[k] >= pa.coor_malla[i] and x[k] <= pa.coor_malla[i + 1]:
-            charge_density[i] = pa.carga_e*(1-abs(x[k]-pa.coor_malla[i])/pa.dx)
+    for k in range (pa.noParticulas):
+        xa = x[k]/pa.dx
+        j1 = int(xa)
+        j2 = j1 + 1
+        f2 = xa - j1
+        f1 = 1.0 - f2
+        charge_density[j1] = charge_density[j1] + re*f1
+        charge_density[j2] = charge_density[j2] + re*f2
+        """
+        if int(x[k]/pa.dx)>= pa.coor_malla[i] and int(x[k]/pa.dx) <= pa.coor_malla[i + 1]:
+            charge_density[i] = charge_density[i] + pa.carga_e*(1-i-int(x[k]/pa.dx))
+            charge_density[i + 1] = charge_density[i + 1] + pa.carga_e*(int(x[k]/pa.dx)-i)
             k = k + 1
         elif x[k] > pa.coor_malla[i + 1]:
             i = i + 1
     charge_density[0] = charge_density[0] + charge_density[pa.noMalla]
     charge_density[pa.noMalla] = charge_density[0]
+    """
 
     return charge_density
 
@@ -166,3 +186,30 @@ def totalenergy (totalenergy,k,u):
     for i in range(pa.noParticulas):
         totalenergy[i] = k[i] + u[i]
     return totalenergy
+
+
+#Funciones para graficar
+
+def diagnosticos(densidad, E, t, graf):
+    xgrid =pa.dx*np.arange(pa.noMalla + 1)
+    if t == 0:
+        plt.figure('Campos')
+        plt.clf()
+        if graf > 0:
+            if np.fmod(t,graf) == 0:
+                #densidad
+                plt.subplot(2,2,1)
+                if t > 0:
+                    plt.cla()
+                plt.plot(xgrid, -(pa.densidadI + densidad),'r', label = 'densidad')
+                plt.xlabel('x')
+                plt.xlim(0,pa.malla_longitud)
+                #Campo electrico
+                plt.subplot(2,2,2)
+                if t > 0:
+                    plt.cla()
+                plt.plot(xgrid, E, 'b', label = 'Campo electrico')
+                plt.xlabel('x')
+                plt.xlim(0,pa.malla_longitud)
+                plt.show()
+    return True
--- Corrección 1/plasma frio.py
+++ Corrección 1/plasma frio.py
@@ -14,126 +14,57 @@ import matplotlib.pyplot as plt
 #Ciclo inicial. Este solo se hace una vez
 
 
-x_inicial = f.buildgrid_pos(pa.x_inicial) #Solo se hace una vez
-print(len(x_inicial))
-v_inicial = f.buildgrid_vel() #Solo se hace una vez
-print(len(v_inicial))
-x_inicial = f.leapfrog(x_inicial,v_inicial)
-densidad_inicial = f.chargedensity(x_inicial)
-E_inicial = f.electricfield(densidad_inicial)
-E_particulas_inicial = f.FieldParticle(x_inicial,E_inicial)
-#K_inicial = f.Kenergy(pa.ki,pa.v_inicial)
+posicion = f.buildgrid_pos(pa.x_inicial) #Solo se hace una vez
+velocidad = f.buildgrid_vel() #Solo se hace una vez
+posicion = f.leapfrog(posicion,velocidad)
+posicion = f.cf(posicion)
+densidad = f.chargedensity(posicion)
+E = f.electricfield(densidad)
+#E_particulas_inicial = f.FieldParticle(posicion,E)
+K = f.Kenergy(velocidad)
 #print (len(K_inicial))
 #U_inicial = f.Uenergy(E_particulas_inicial,pa.upot)
 #print (len(U_inicial))
-"""
-He tenido problemas con la funcion de energia potencial Uenergy. Me tira un Index error,
-pero cuando comparo la cantidad de particulas que analiza la funcion que cree para analizar
-los E de cada particula-FieldParticle- entonces aun no se porque tira ese error.
-"""
-
-
-
 
 #Se empieza a constuir el ciclo
 
-
-i = 0
+xgrid =pa.dx*np.arange(pa.noMalla + 1)
+graf = (np.pi/pa.dt/16)
+t = 0
 p = 0
 e = 1
-posiciones= []
-velocidades = []
-densidades = []
-camposE = []
-camposE_particulas = []
-tiempo = []
-energiacinetica = []
+posiciones= [posicion]
+velocidades = [velocidad]
+densidades = [densidad]
+camposE = [E]
+#camposE_particulas = [E_particulas_inicial]
+tiempo = [0]
+energiacinetica = [K]
 #energiapotencial = []
 #energiatotal = []
 #kdrift = []
 
 
 
-while  i < 0.3:
-    if i == 0:
-        posicion = f.chargeposition( v_inicial,x_inicial)
-        posicion = f.cf(posicion)
-        velocidad = f.chargevelocity(x_inicial, v_inicial, E_inicial)
-        densidad = f.chargedensity(posicion)
-        E = f.electricfield(densidad)
-        K = f.Kenergy(v_inicial)
-        posiciones = np.append(posiciones, posicion)
-        velocidades = np.append(velocidades,velocidad)
-        densidades = np.append(densidades, densidad)
-        camposE = np.append(camposE, E)
-        energiacinetica = np.append(energiacinetica, K)
-        #camposE_particulas.append([v/(pa.carga_e*pa.dt) for v in velocidades[j]])
-    else:
-        posicion = f.chargeposition(velocidad, posicion)
-        posicion = f.cf(posicion)
-        velocidad = f.chargevelocity(posicion, velocidad, E)
-        densidad = f.chargedensity(posicion)
-        E = f.electricfield(densidad)
-        K = f.Kenergy(velocidad)
-        posiciones = np.append(posiciones, posicion)
-        velocidades = np.append(velocidades, velocidad)
-        densidades = np.append(densidades, densidad)
-        camposE = np.append(camposE, E)
-        energiacinetica = np.append(energiacinetica, K)
-        p = p + 1 #Cuando agrego esto mi tira error:
-
-
-        #camposE_particulas.append([v/(pa.carga_e*pa.dt) for v in velocidades[j]])
-
-    i = i + pa.dt
-    tiempo.append(i)
+while  t < 0.6:
+    posicion = f.chargeposition( velocidad,posicion)
+    posicion = f.cf(posicion)
+    velocidad = f.chargevelocity(posicion, velocidad, E)
+    densidad = f.chargedensity(posicion)
+    E = f.electricfield(densidad)
+    K = f.Kenergy(velocidad)
+    posiciones = np.append(posiciones, posicion)
+    velocidades = np.append(velocidades,velocidad)
+    densidades = np.append(densidades, densidad)
+    camposE = np.append(camposE, E)
+    energiacinetica = np.append(energiacinetica, K)
+    f.diagnosticos(densidad, E,t,graf)
+    t = t + pa.dt
+    tiempo.append(t)
+    print(velocidades)
 
 posiciones = [posiciones[i:i + pa.noParticulas] for i in range (0, len(posiciones),pa.noParticulas)]
 velocidades = [velocidades[i:i + pa.noParticulas] for i in range (0, len(velocidades),pa.noParticulas)]
 camposE = [camposE[i:i+pa.noMalla+1] for i in range(0,len(camposE),pa.noMalla+1)]
 densidades = [densidades[i:i+pa.noMalla+1] for i in range(0,len(densidades),pa.noMalla+1)]
 energiacinetica = [energiacinetica[i:i+pa.noParticulas] for i in range (0,len(energiacinetica),pa.noParticulas)]
-"""
-print ('posiciones')
-print ( posiciones)
-print (len(posiciones))
-print ('velocidades')
-print (  velocidades)
-print (len(velocidades))
-print ('densidades')
-print ( densidades)
-print (len(densidades))
-print ('campos')
-print (camposE)
-"""
-
-#Campo electrico en el tiempo
-"""
-grafE = []
-for i in range(len(camposE)):
-    grafE.append(camposE[i][0]) #nodo 0
-plt.plot(tiempo,grafE)
-plt.show()
-"""
-"""
-grafK = []
-for i in range(len(energiacinetica)):
-    grafK.append(energiacinetica[i][i])
-plt.plot(tiempo, grafK)
-plt.show()
-"""
-
-#Diagramas de fase de la particula 0
-x = []
-y = []
-
-#xgrid = pa.dx*np.arange(pa.noMalla + 1)
-#plt.scatter(xgrid, -(pa.densidadI + densidades[3]))
-
-#plt.scatter(xgrid, camposE[1])
-#plt.xlim(0,pa.malla_longitud)
-#plt.ylim(-1,-0.75)
-#plt.show()
-print (len(densidades))
-print (posiciones[1])
-print(velocidades[1])
